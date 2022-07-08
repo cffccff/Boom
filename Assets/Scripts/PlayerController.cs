ede3a6d83b28abb2e42b2551f618577f4fef07c9
Kiet1994 <phanhoangkiet1994@gmail.com>
2022-07-08 20:38:19 +0700

    fix Player Move, add asset sprite.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,13 +15,13 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)// nếu khoảng cách nhỏ hơn 0.05 khi bấm di chuyển
         {
-            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) //nếu bấm nút đạt tới -1,1 
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"),0f,0f), .2f, whatStopsMovement))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"),0f,0f), .2f, whatStopsMovement))// tâm cách player 1 đơn vị và bán kính là 0,2 nên giúp chỉ phát hiện duy nhất 1 vật cản.
                 {
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);// nếu không phát hiện vật cản thì point dịch chuyển tới vị trí mới từ đó player di chuyển dần tới point
                 }
             }
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
