119b33977075f202bf90098084a4abcc710bf9bf
cffccff <73158668+cffccff@users.noreply.github.com>
2022-07-16 18:55:55 +0700

    add random direction for a enemy when not collide

--- Assets/Scripts/Game.Runtime/Enemy/Pakupa.cs
+++ Assets/Scripts/Game.Runtime/Enemy/Pakupa.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class Pakupa : BaseEnemy
 {
-   
+    float count;
+    
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+         base.OnTriggerEnter2D(collision);
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Item"))
+        {
+            Debug.Log("Item collider");
+            Destroy(collision.gameObject);
+        }
+    }
+    protected override void Update()
+    {
+        base.Update();
+        count += Time.fixedDeltaTime;
+
+        if (count >= 60)
+        {
+            Debug.Log("count " + count);
+            Debug.Log("change");
+            directionChosen = false;
+            count = 0;
+        }
+    }
 }
