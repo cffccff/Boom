29e6f30e010fb6725667354448ce494afc321bc4
Kiet1994 <phanhoangkiet1994@gmail.com>
2022-07-23 09:34:16 +0700

    fix map boss

--- Assets/Scripts/Game.Runtime/LevelMap/LevelManager.cs
+++ Assets/Scripts/Game.Runtime/LevelMap/LevelManager.cs
@@ -94,7 +94,7 @@ public class LevelManager : MonoBehaviour
 
         //save the data as a json
         string json = JsonUtility.ToJson(levelData, true);
-        File.WriteAllText(Application.dataPath + "/Resources/Levels/Level12.json", json);
+        File.WriteAllText(Application.dataPath + "/Resources/Levels/Level13.json", json);
 
         //debug
         Debug.Log("Level was saved");
